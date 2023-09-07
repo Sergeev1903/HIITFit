80b56bbe03eea579fa72537ab810756498dc39bd
Artem Sergeev <sergeev190391@gmail.com>
2023-09-07 19:57:46 +0300

    finish Chapter 3

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -1,15 +1,45 @@
 
 import SwiftUI
+import AVKit
 
 struct ExerciseView: View {
-
-  let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
-  let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+  
+  let videoNames = [
+    "squat", "step-up", "burpee", "sun-salute"
+  ]
+  let exerciseNames = [
+    "Squat", "Step Up", "Burpee", "Sun Salute"
+  ]
   let index: Int
-
-
+  let interval: TimeInterval = 30
+  
   var body: some View {
-    Text(exerciseNames[index])
+    GeometryReader { geometry in
+      VStack {
+        VStack {
+          HeaderView(exerciseName: exerciseNames[index])
+          if let url = Bundle.main.url(
+            forResource: videoNames[index],
+            withExtension: "mp4") {
+            VideoPlayer(player: AVPlayer(url: url))
+              .frame(height: geometry.size.height * 0.45)
+          } else {
+            Text("Couldn’t find \(videoNames[index]).mp4")
+              .foregroundColor(.red)
+          }
+          Text(Date().addingTimeInterval(interval), style: .timer)
+            .font(.system(size: 90))
+          Button("Start/Done") { }
+            .font(.title3)
+            .padding()
+          RatingView()
+            .padding()
+          Spacer()
+          Button("History") { }
+            .padding(.bottom)
+        }
+      }
+    }
   }
 }
 
@@ -18,3 +48,5 @@ struct ExerciseView_Previews: PreviewProvider {
     ExerciseView(index: 0)
   }
 }
+
+
